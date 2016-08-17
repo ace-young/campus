154c683212ec1b729e214c245bf61d4cdf2a1fbb
lwh <lwhile@outlook.com>
2016-08-17 15:03:56 +0800

    style

--- android_app/index.android.js
+++ android_app/index.android.js
@@ -1,34 +1,29 @@
-import React, {Component} from 'react';
-import {AppRegistry, Image,Text,View} from 'react-native';
-
-
-class Blink extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {showText:true};
-
-        //每1000毫秒对showText状态做一次取反操作
-        setInterval(() => {
-            this.setState({showText:!this.state.showText});
-        }, 2000);
-    }
-
-    render() {
-        let display = this.state.showText ? this.props.text : ' ';
-        return (
-            <Text>{display}</Text>
-        );
-    }
-}
+import React, {Component} from 'react'
+import {AppRegistry, StyleSheet, Text, View} from 'react-native'
 
 class AwesomeProject extends Component {
     render() {
         return (
             <View>
-                <Blink text='LWH' />
-                <Blink text='a'/>
+                <Text style={styles.red}>just red</Text>
+                <Text style={styles.bigblue}>just bigblue</Text>
+                <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+                <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
             </View>
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+    bigblue:{
+        color:'blue',
+        fontWeight:'bold',
+        fontSize:30,
+    },
+    red:{
+        color:'red',
+    },
+});
+
 AppRegistry.registerComponent('AwesomeProject', ()=>AwesomeProject)
